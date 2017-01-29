9757ed5c52b32d461a160b93ad252580f3efb65a
Cameron Poe <cameron@zay.li>
2017-01-28 22:24:23 -0500

    first commit

---
+++ zay.li/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.StaticFile("/style.css", "./static/css/style.css")
+  r.StaticFile("/image.gif", "./static/images/image.gif")
+  r.StaticFile("/music.mp3", "./static/music.mp3")
+  r.StaticFile("/music2.mp3", "./static/music2.mp3")
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+	r.GET("/ssh", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "key.html", gin.H{})
+	})
+
+	// Custom Server Info
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
